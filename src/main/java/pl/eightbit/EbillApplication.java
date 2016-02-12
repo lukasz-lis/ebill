7990d5cbd797823ea4c7c1a2410534ae790b247f
Łukasz Lis <lukasz.lis@windowslive.com>
2016-02-12 23:05:06 +0100

    Initial commit

---
+++ src/main/java/pl/eightbit/EbillApplication.java
@@ -0,0 +1,12 @@
+package pl.eightbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbillApplication.class, args);
+	}
+}
