583ba982c6379318cc3e69756525d627efca3b5f
Lukasz Lis <lukasz.lis@windowlive.com>
2017-01-22 13:47:57 +0100

    Receipt API

--- src/main/java/pl/eightbit/api/ReceiptRestController.java
+++ src/main/java/pl/eightbit/api/ReceiptRestController.java
@@ -2,14 +2,18 @@ package pl.eightbit.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.eightbit.dao.TokenRepository;
 import pl.eightbit.dto.ReceiptDTO;
 import pl.eightbit.dto.ReceiptDetailsDTO;
+import pl.eightbit.models.Token;
 import pl.eightbit.services.ReceiptService;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -17,6 +21,9 @@ public class ReceiptRestController {
 
     @Autowired
     private ReceiptService receiptService;
+
+    @Autowired
+    private TokenRepository tokenRepository;
 
     @RequestMapping(value = "/receipt/{pageNumber}/{pageSize}", method = RequestMethod.GET)
     public ResponseEntity<List<ReceiptDTO>> createReceipt(@PathVariable(value = "pageNumber") final int pageNumber, @PathVariable(value = "pageSize") final int pageSize) {
@@ -32,17 +39,17 @@ public class ReceiptRestController {
         return ResponseEntity.ok(receiptDetailsDTO);
     }
 
-    @RequestMapping(value = "/receipt-details/", method = RequestMethod.POST)
-    public ResponseEntity<Long> createReceipt(@Valid @RequestBody final ReceiptDetailsDTO receiptDetailsDTO) {
+    @RequestMapping(value = "/receipt-details/{userToken}", method = RequestMethod.POST)
+    public ResponseEntity<Long> createReceipt(@Valid @RequestBody final ReceiptDetailsDTO receiptDetailsDTO, @PathVariable(value = "userToken") final String userToken) {
+
+        final Optional<Token> token = tokenRepository.findByToken(userToken);
+        if (!token.isPresent()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(-1L);
+        }
 
         final long receiptID = receiptService.saveReceiptDetailsDTO(receiptDetailsDTO);
+        tokenRepository.delete(token.get().getId());
         return ResponseEntity.ok(receiptID);
-    }
-
-
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public ResponseEntity<String> createReceipt() {
-        return ResponseEntity.ok("OK");
     }
 
 
